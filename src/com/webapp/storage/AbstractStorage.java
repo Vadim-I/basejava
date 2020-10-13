d4f92cc121d0d022faf5b5b052c5541f961f79b6
Vadim-I <isaeff@bk.ru>
2020-10-13 19:19:20 +0700

    Lesson_7 Logging

--- src/com/webapp/storage/AbstractStorage.java
+++ src/com/webapp/storage/AbstractStorage.java
@@ -6,10 +6,15 @@ import com.webapp.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
 
+//    protected final Logger log = Logger.getLogger(getClass().getName());
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+
     public void update(Resume resume) {
+        LOG.info("Update " + resume);
         SK key = getExistedKey(resume.getUuid());
         updateResume(resume, key);
     }
@@ -17,6 +22,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     protected abstract void updateResume(Resume resume, SK key);
 
     public void save(Resume resume) {
+        LOG.info("Save " + resume);
         SK key = getNotExistedKey(resume.getUuid());
         saveResume(resume, key);
     }
@@ -24,6 +30,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     protected abstract void saveResume(Resume resume, SK key);
 
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK key = getExistedKey(uuid);
         return getResume(key);
     }
@@ -31,6 +38,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     protected abstract Resume getResume(SK key);
 
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK key = getExistedKey(uuid);
         deleteResume(key);
     }
@@ -38,6 +46,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     protected abstract void deleteResume(SK key);
 
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> list = copyResumeList();
         Collections.sort(list);
         return list;
@@ -48,6 +57,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getExistedKey(String uuid) {
         SK key = getKey(uuid);
         if (!isExist(key)) {
+            LOG.warning("Resume " + uuid + " is not exist");
             throw new NotExistStorageException(uuid);
         }
         return key;
@@ -56,6 +66,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getNotExistedKey(String uuid) {
         SK key = getKey(uuid);
         if (isExist(key)) {
+            LOG.warning("Resume " + uuid + " is exist already");
             throw new ExistStorageException(uuid);
         }
         return key;
