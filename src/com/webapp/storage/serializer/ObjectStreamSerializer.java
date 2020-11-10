cfb1d25d575b34a5494ff08ebf5bb51316396e2c
Vadim-I <isaeff@bk.ru>
2020-11-10 11:37:17 +0700

    Lesson_10 realization in xml

---
+++ src/com/webapp/storage/serializer/ObjectStreamSerializer.java
@@ -0,0 +1,25 @@
+package com.webapp.storage.serializer;
+
+import com.webapp.exception.StorageException;
+import com.webapp.model.Resume;
+
+import java.io.*;
+
+public class ObjectStreamSerializer implements StreamSerializer {
+
+    @Override
+    public void doWrite(Resume resume, OutputStream os) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(os)) {
+            oos.writeObject(resume);
+        }
+}
+
+    @Override
+    public Resume doRead(InputStream is) throws IOException {
+        try (ObjectInputStream ois = new ObjectInputStream(is)) {
+            return (Resume) ois.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new StorageException("Read resume error", null, e);
+        }
+    }
+}
---
+++ src/com/webapp/storage/serializer/StreamSerializer.java
@@ -0,0 +1,15 @@
+package com.webapp.storage.serializer;
+
+import com.webapp.model.Resume;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public interface StreamSerializer {
+
+    void doWrite(Resume resume, OutputStream os) throws IOException;
+
+    Resume doRead(InputStream is) throws IOException;
+}
